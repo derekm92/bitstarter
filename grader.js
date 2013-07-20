5a91ee765df6d1f768cb2ce14c64b20fa5853a30
Derek Miller <derekm@gatech.edu>
2013-07-20 02:25:30 +0000

    index.html themed

--- grader.js
+++ grader.js
@@ -24,12 +24,21 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
+	rest.get(instr).on('complete', function(result) {
+  		if (result instanceof Error) {
+    			sys.puts('Error: ' + result.message);
+  		} else {
+    			return instr;
+  		}
+	});
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
